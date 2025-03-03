e1344710e23da20f4506c2a49cb2e475f6e27b92
zaphir <ianice.lange@supdevinci-edu.fr>
2025-03-03 14:39:55 +0100

    Suppression du probleme des platforms

--- front/function/createPlatform.js
+++ front/function/createPlatform.js
@@ -1,17 +1,17 @@
 import {createBlock} from "./createBlock.js";
-import {movementLeft} from "./movementLeft.js";
+import {movementLeftSol} from "./movementLeft.js";
 let platforms = [];
 let j
 export async function createPlatform(blockSize, app, PIXI) {
-   
+
             if(Math.random() > 0.98){
                 let lastPlatformX = platforms.length > 0 ? platforms[platforms.length - 1].x : -1;
-            let minSpacing = blockSize * 10; // Espace minimum entre plateformes
+            const minSpacing = blockSize * 10; // Espace minimum entre plateformes
+            
             // Vérifie que la nouvelle plateforme ne soit pas collée à la précédente
             if (lastPlatformX !== -1 && lastPlatformX > app.screen.width - minSpacing) {
                 return;
             }
-
             let hauteur = Math.floor(Math.random() * 3) + 4;
             let longueur= Math.floor(Math.random() * 4) + 3;
                 for(let i = 0; i < longueur; i++){
@@ -23,6 +23,6 @@ export async function createPlatform(blockSize, app, PIXI) {
                         
         
     
-    platforms = await movementLeft(platforms, app);
+    platforms = await movementLeftSol(platforms, app);
     return platforms;
 }
